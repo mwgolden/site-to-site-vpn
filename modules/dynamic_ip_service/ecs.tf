ab40aa9d93a3e315bde07a3bc2262d32c9ef5f19
matthew <matthew@wgolden.com>
2024-08-17 09:51:02 -0500

    First draft of ecs task

---
+++ modules/dynamic_ip_service/ecs.tf
@@ -0,0 +1,32 @@
+data "aws_caller_identity" "account" {}
+
+resource "aws_ecs_cluster" "ecs_cluster" {
+    name = "dyn-ip-svc-cluster"
+}
+
+ resource "aws_ecs_task_definition" "terraform_runner" {
+    family = "terraform-runner-task"
+    requires_compatibilities = [ "FARGATE" ]
+    network_mode = "awsvpc"
+    cpu = 256
+    memory = 512
+    container_definitions = templatefile("${path.module}/ecs-tf-runner.def.json", { account_number = data.aws_caller_identity.account.account_id })
+    execution_role_arn = aws_iam_role.ecs_role.arn
+    task_role_arn = aws_iam_role.ecs_role.arn
+ }
+
+ resource "aws_iam_role" "ecs_role" {
+  name               = "ecs_task_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_assume_role.json
+}
+
+resource "aws_iam_policy" "ecs_policy" {
+  name        = "aws_ecs_policy"
+  description = "AWS IAM Policy for managing aws ecs role"
+  policy      = data.aws_iam_policy_document.ecs_task_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach_ecs_iam_policy_to_role" {
+  role       = aws_iam_role.ecs_role.name
+  policy_arn = aws_iam_policy.ecs_policy.arn
+}
--- modules/dynamic_ip_service/iam_policy.tf
+++ modules/dynamic_ip_service/iam_policy.tf
@@ -44,3 +44,39 @@ data "aws_iam_policy_document" "lambda_policy" {
     resources = ["*"]
   }
 }
+
+data "aws_iam_policy_document" "ecs_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "ecs_task_role" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:List*"
+    ]
+    resources = [ "arn:aws:s3:::com.wgolden.tfstate/*" ]
+  }
+  
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [ "*" ]
+  }
+}
